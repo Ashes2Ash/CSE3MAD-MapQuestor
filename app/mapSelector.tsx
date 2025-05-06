e9a3ec1c96130f8ffa77cc1c4edc4a6b85e7c5a3
Ashes2Ash <ash.meaney02@gmail.com>
2025-05-07 00:17:19 +1000

    added maptiles to the map selection screen, will follow with a grid component. all commented, changed some config options due to typescript rules being too strict

--- app/mapSelector.tsx
+++ app/mapSelector.tsx
@@ -1,7 +1,76 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-// Expo Router navigation object for screen transitions
-import { router } from 'expo-router';
+import React, {FC} from 'react'; //Imports react and Function Component
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'; //Imports building blocks for screen
+import {useState,useEffect} from 'react'; //useState stores states of data. used in this to hold map lists from firebase. useEffect runs side loaded scripts at specific times 
+import {auth,db} from '../firebaseConfig'; //auth allows us to confirm which user is logged in, db links to our firestore
+import { router } from 'expo-router'; //redirects to other page in app
+import {collection, getDocs} from 'firebase/firestore'; //allows us to explore data collections and documents stored in firebase storage, used here to get map links for the tiles.
+
+//Lists properties of a MapTile which is made up fo the image uri (link to image), title of map (user provided name of map), and what to do on click. The Key prop refers to an internal identifier that typically doesnt need to be declared but due to strict rules on Typescript i have done so.
+interface MapTileProps {
+  uri:     string;
+  title:   string;
+  onPress: () => void;
+  key?: React.Key;
+}
+//Reusable component (FC: Functional Component), made up of the above properties that is looped through below to list out each map attached to the user in a grid.
+const MapTile: FC<MapTileProps> =({uri, onPress,title})=>{
+  return(
+    <TouchableOpacity onPress={onPress}>
+      <Image
+      source={{uri}}
+      style={{ width: 100, height: 100}}
+      ></Image>
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
 export default function mapSelector() {
+  //defines the maps array that holds the users map list. the setMaps function that is called to update the array
+  const [maps, setMaps]= useState([]as {
+    id: string;
+    uri: string;
+    title: string;
+    onPress: () => void;
+    }[]);
+    //useEffect side loads data once, right after the screen loads.
+  useEffect(()=>{
+    async function getMaps(){
+      //confirms user is signed in and retreives their uid (userId)
+      const user= auth.currentUser;
+      if(!user){
+        return
+      }
+      const uid=user.uid
+      console.log(user)
+      //uses the uid to search for that users maps in the firestore collection at the path db/users/{uid}/maps
+      const mapCollection = collection(db,"users/"+uid+"/maps/")
+      //gets all of the users map data into a snapshot
+      const snapshot=await getDocs(mapCollection);
+      //logs map totals tied to user (for testing)
+      console.log("Number of maps on account: "+snapshot.size)
+      //converts each map document into a js object and pushes each object to the below mapList array
+      const mapList=[]
+      for(let docSnap of snapshot.docs){
+        let data= docSnap.data()
+        console.log(data)
+        mapList.push({
+          id:       docSnap.id,
+          uri:      data.imageURL,
+          title:    data.title,
+
+          onPress: () => console.log("Tapped", docSnap.id) //Just alerts console that tap has been registered, update when ready(NOTHING WHILE STILL IN DEVELOPMENT)
+        })
+        
+      }
+      //sets the maps State variable as the received mapList
+      setMaps(mapList)
+    }
+    //calls the getMaps() useEffect function when the page is loaded.
+    getMaps()
+  },[]);// the [] means there are no dependencies, so the useEffect code runs only once when the component is first loaded.
+  
+  
   return (
     <View style={styles.screen}>
   <View style={styles.title}>
@@ -9,7 +78,17 @@ export default function mapSelector() {
   </View>
 
   <View style={styles.container}>
-    <Text>place map tiles here</Text>
+
+  {maps.map(item => (
+    <MapTile
+      key={item.id}
+      uri={item.uri}
+      onPress={item.onPress}
+      title={item.title}
+      
+    />
+  ))}
+
   </View>
   <View>
     <TouchableOpacity style={styles.testButton} onPress={() => router.push('/uploadMap')}>
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,7 +22,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app); 
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 export const auth = getAuth(app); 
 const storage = getStorage(app); // Initialize Storage
 
